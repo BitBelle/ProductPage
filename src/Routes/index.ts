6ff32490a19fdb3b857e86cbeefdb0c0351f4751
BitBelle <wainainamercy432@gmail.com>
2024-06-10 21:51:15 +0300

    Initial routes

---
+++ src/Routes/index.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import {updateProduct,addProduct,getProduct,getProducts, deleteProduct} from '../Controllers'
+
+const router=Router()
+router.get('',getProducts)
+ router.get('/:id',getProduct)
+router.post('',addProduct)
+router.put('/:id',updateProduct)
+router.delete('/:id',deleteProduct)
+
+
+export default router
